69bb26bf43a4c2f3032f72d3c5e343853d8e540c
rmm1999 <rmm0029@mix.wvu.edu>
2021-01-26 19:34:07 -0500

    Ryan McClure Task Upload b

--- Tasks_01/Tasks_01.r
+++ Tasks_01/Tasks_01.r
@@ -1,3 +1,16 @@
-install.packages ("swirl")
-library (swirl)
-
+5+7
+x <- 5+7
+x
+y <- x-3
+y
+z <- c(1.1, 9, 3.14)
+?c
+z
+c(z, 555, z)
+z * 2 + 100
+my_sqrt <- sqrt(z - 1)
+my_sqrt
+my_div <- z / my_sqrt
+my_div
+c(1, 2, 3, 4) + c(0, 10)
+c(1, 2, 3, 4) + c(0, 10, 100)
