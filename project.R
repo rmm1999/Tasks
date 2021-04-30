7994f9f96a5367465e03dd39f49a6fe3bac97c1c
rmm1999 <rmm0029@mix.wvu.edu>
2021-04-29 21:58:38 -0400

    Ryan McClure Task 10

--- project.R
+++ project.R
@@ -1,12 +1,23 @@
 setwd("C:\\Users\\12345\\Desktop\\Evolution")
 #Data <- read.csv("lizard_data.csv")
-Data <- read.csv("project_data.csv")
+Data <- read.csv("new_project_data.csv")
 
 head(Data)
 
-Data$Season <- gsub("early", "Early", Data$Season)
+# Edit these to change the point characters
+PCHs <- c(15, 16, 1, 5, 17, 18, 20, 6, 2)
 
-Surv <- tapply(Data$Survival, Data$Season, function(x) sum(x)/length(x))
+# Edit these to change the colors
+Colors <- c('#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999')
 
-par(mar=c(4,5,1,1), las=1, mgp=c(2.5, 0.25, 0), tck=-0.01)
-barplot(Surv, names.arg = c("Early", "Late"), col="white")
+par(mar=c(4,5,1,1), mgp=c(2.5, 0.5, 0), las=1, tck=-0.01)
+plot(c(Data$SVL,65), c(Data$Latitude,20), type="n", xlab="Snout Vent Length", ylab="Latitude")
+
+Specs <- unique(Data$Species)
+for (i in 1:length(Specs)){
+  Chosen <- which(Data$Species == Specs[i])
+  points(Data$SVL[Chosen], Data$Latitude[Chosen], pch=PCHs[i], col=Colors[i])
+}
+
+legend("topright", col=Colors, pch=PCHs, legend=Specs, bty="n")
+
